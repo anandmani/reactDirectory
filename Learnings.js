0347ac82f3b1dd64ac7ef9f2b8f1643a307c2bc3
Anand Mani <cma.camelot@gmail.com>
2016-11-28 23:54:58 +0530

    Adding comments related to using relative pathing while using react-router and how it is not in harmony with hosting on github

--- Learnings.js
+++ Learnings.js
@@ -30,3 +30,14 @@
  //
  // The KeyPress event --> If the textArea has "AB" and we press 'C',  event.keyCode will return 0!!, event.target.value will return "AB"!!
  //Order of events: keydown > keypress > textInput  >   keyup
+
+
+//When using react-router, We need to change our relative paths to absolute paths in index.html since the URLs will be at deep paths and the app, if it starts at a deep path, won't be able to find the files.
+//Refer https://github.com/reactjs/react-router-tutorial/tree/master/lessons/10-clean-urls
+// cd / command takes us to root (absolute path from root)
+// cd command without leading / is used for relative path (from current directory)
+//Same logic applies for react Router
+//I'm not changing the path to absolute path in index.html because, when we host using github pages, it automatically adds projectname to the base path. Thus, we cant reference files properly since absolute pathing will remove this project name from path
+//e.g. Ideally, https://anandmani.github.io/ should be the base path
+// But, github makes it https://anandmani.github.io/reactDirectory/ by appending reactDirectory/
+//If we want to access a file in /src/js from base path, on github pages we need to use https://anandmani.github.io/reactDirectory/src/js   But, absolute pathing will make it https://anandmani.github.io/src/js where nothing is present.
